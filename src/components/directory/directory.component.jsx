6bdc6af77238ecdbe42d6488d996efd2daf4c00e
m0nq <forgetmenaught@gmail.com>
2023-01-12 17:39:39 -0800

    Installed Open Sans font in project

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,5 @@
 import { CategoryItem } from '../category-item/category-item.component';
-import './directory.styles.scss'
+import './directory.styles.scss';
 
 export const Directory = ({ categories }) => {
     return (

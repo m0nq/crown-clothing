483604528f0d01bddb1a871953e1a50a02dfea93
m0nq <forgetmenaught@gmail.com>
2023-05-03 13:33:05 -0700

    Refactor user context to use reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,5 @@
+import { useReducer } from 'react';
 import { useEffect } from 'react';
-import { useState } from 'react';
 import { createContext } from 'react';
 
 import { createUserDocument } from '../utils/firebase/firebase.utils';
@@ -10,8 +10,35 @@ export const UserContext = createContext({
     currentUser: null
 });
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+};
+
+const userReducer = (state, action) => {
+    console.log('dispatched');
+    console.log(action);
+    const { type, payload } = action;
+
+    switch (type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload
+            };
+        default:
+            return state;
+    }
+
+};
+
+const INITIAL_STATE = {
+    currentUser: null
+};
+
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE, undefined);
+    console.log(currentUser);
+    const setCurrentUser = user => dispatch({ type: 'USER_ACTION_TYPES.SET_CURRENT_USER', payload: user });
     const value = { currentUser, setCurrentUser };
 
     useEffect(() => {
